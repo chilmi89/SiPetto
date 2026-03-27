7abbdb8ba9e5d793a999ddd9457210fd76404603
shynix89 <achmadchilmi87@gmail.com>
2026-03-27 11:15:42 +0700

    update error storage upload
    update error storage upload

--- src/app/api/upload/avatar/route.ts
+++ src/app/api/upload/avatar/route.ts
@@ -5,8 +5,8 @@ export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
   try {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-    const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || '';
+    const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || '';
 
     if (!supabaseUrl || !supabaseKey) {
         console.error("EROR KONFIGURASI SUPABASE:", { 
